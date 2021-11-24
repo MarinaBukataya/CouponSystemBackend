68eb6b364e4cc4b16edb3c1a30eee67dab4494d8
MarinaBukataya <mbukataya@yandex.ru>
2021-11-24 12:49:33 +0200

    Removed unused imports

--- src/main/java/com/johnbryce/cs/beans/Company.java
+++ src/main/java/com/johnbryce/cs/beans/Company.java
@@ -5,17 +5,12 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
